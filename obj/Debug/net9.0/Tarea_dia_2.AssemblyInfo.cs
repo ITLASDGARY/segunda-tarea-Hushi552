ef8b1f7e2f1ba17c3ea373b9b01d897a1f819872
Hushi552 <jeanc552460@hotmail.com>
2026-02-05 22:27:15 +0000

    tarea 2 completada

--- obj/Debug/net9.0/Tarea_dia_2.AssemblyInfo.cs
+++ obj/Debug/net9.0/Tarea_dia_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8bf70cbe97caf0c074427784e98e8cfd4cac146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d189a31191a5113e13839d1c3768255e66d8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_dia_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
